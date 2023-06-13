7bff304652a2c51f7e97a7fcff64e207ce12bf91
lcastiglione <leonardocastiglione@hotmail.com>
2023-06-13 09:18:40 -0300

    Add abstract http class

---
+++ pipeline/core/http/exceptions.py
@@ -0,0 +1,14 @@
+﻿"""
+Módulo que contiene los tipos de excepciones que pueden ocurrir en peticiones http.
+"""
+
+
+class ResponseHTTPException(Exception):
+    """_summary_
+
+    Args:
+        Exception (_type_): _description_
+    """
+
+    def __init__(self, msg,url,method):
+        super().__init__(f"Se produjo un error al procesar la url '{url}' con el metodo '{method}': {msg}")
---
+++ pipeline/core/http/httpclient.py
@@ -0,0 +1,24 @@
+﻿"""Módulo con las clase abtsracta que deifne los métodos que tienen que tener todas las implementaciones que hacen peticiones HTTP"""
+
+import abc
+from typing import Any, Dict, Optional
+import aiohttp
+
+
+class AbstractHttpClient(abc.ABC):
+
+    @abc.abstractmethod
+    async def get(self, url: str, params: Optional[Dict[str, Any]] = None, **kwargs: Any) -> Any:
+        pass
+
+    @abc.abstractmethod
+    async def post(self, url: str, data: Optional[Dict[str, Any]] = None, **kwargs: Any) -> Any:
+        pass
+
+    @abc.abstractmethod
+    async def put(self, url: str, data: Optional[Dict[str, Any]] = None, **kwargs: Any) -> Any:
+        pass
+
+    @abc.abstractmethod
+    async def delete(self, url: str, **kwargs: Any) -> Any:
+        pass
